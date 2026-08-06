fd48c8fe28ea995378efc7cbd49969fd1d47b827
MHaseef <m.haseef135@gmail.com>
2026-08-06 14:24:09 +0500

    Enable Astro SSR mode for Vercel OAuth

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 import react from '@astrojs/react';
 import decapCmsOauth from 'astro-decap-cms-oauth';
 
-// https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
   integrations: [react(), decapCmsOauth()],
 });
+
